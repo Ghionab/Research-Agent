6b0015419bfcdaa085d8fe856f6146183aa3591d
Ghionab <abrahammulualem3@gmail.com>
2025-11-24 23:38:28 +0300

    1st commit

---
+++ main.py
@@ -0,0 +1,66 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
+
+
+
+# Load environment variables
+load_dotenv()
+
+# Define structured response model
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+# Initialize Gemini model
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.3)
+
+# Create output parser
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+# Define prompt
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that helps generate concise, structured research reports.
+            Use tools as needed to gather information.
+            Respond strictly in this format:\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+# Register tools
+tools = [search_tool, wiki_tool, save_tool]
+
+# Create agent
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+# Create executor
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+# Run
+query = input("What can I help you research? ")
+raw_response = agent_executor.invoke({"query": query})
+
+# Parse and display output
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
